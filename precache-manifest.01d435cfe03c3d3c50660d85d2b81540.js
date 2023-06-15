d82d907073d1da8a32442e18ffed1544dddbf05c
Arati0202 <136055021+Arati0202@users.noreply.github.com>
2023-06-15 11:39:20 +0200

    Updates

---
+++ precache-manifest.01d435cfe03c3d3c50660d85d2b81540.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "06d0fdf49d2a02c3d0d1",
+    "url": "/Helloworld/static/css/main.4212926a.chunk.css"
+  },
+  {
+    "revision": "06d0fdf49d2a02c3d0d1",
+    "url": "/Helloworld/static/js/main.06d0fdf4.chunk.js"
+  },
+  {
+    "revision": "3e3c3eb55c09e01a1300",
+    "url": "/Helloworld/static/js/1.3e3c3eb5.chunk.js"
+  },
+  {
+    "revision": "ae6e7b9f0a673c9b4048",
+    "url": "/Helloworld/static/js/2.ae6e7b9f.chunk.js"
+  },
+  {
+    "revision": "93b3e7ef9d4b01283454",
+    "url": "/Helloworld/static/js/runtime~main.93b3e7ef.js"
+  },
+  {
+    "revision": "e56f62275c42f2aeec7ad408dbcdf08d",
+    "url": "/Helloworld/index.html"
+  }
+];
